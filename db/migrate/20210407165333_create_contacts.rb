f628b45e16d0a897802cfa8c08287c0f86240479
Carlos Villa <cvillad@uninorte.edu.co>
2021-04-07 11:56:51 -0500

    add devise, create users and contacts table

---
+++ db/migrate/20210407165333_create_contacts.rb
@@ -0,0 +1,17 @@
+class CreateContacts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :contacts do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.date :born_date
+      t.string :phone
+      t.string :address
+      t.string :credit_card_number
+      t.string :franchise
+      t.string :email
+
+      t.timestamps
+    end
+    add_index :contacts, :email, unique: true
+  end
+end
